2b60884e352c283388edf54a058a1c1a9165c648
William Kunitake <william.kunitake@gmail.com>
2025-01-18 10:58:12 -0800

    Included Pagination, BarLoader and error handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Pagination from "./pagination";
+import Pagination from "./components/Pagination/pagination";
+import { BarLoader } from "react-spinners";
 
 export default function Home() {
-
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(10);
-  const [loading, setLoading] = useState(true);
 
   // setting a new type for the API elements
   type Users = {
@@ -20,6 +17,11 @@ export default function Home() {
     }
   }
 
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [postsPerPage, setPostsPerPage] = useState<number>(10);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
   const [users, setUsers] = useState<Users[]>([]);
 
   // API address
@@ -31,11 +33,15 @@ export default function Home() {
       // using the window.fetch method to get the request
       const res = await window.fetch(API_URL);
       // Throwing error in case the response variable is unavailable
-      if (!res.ok) throw new Error(`HTTP error! Status: ${res.status}`);
-      // transforming the "res" variable to json
-      const json = await res.json();
-      // setting the state
-      setUsers(json.results); // results is the API array
+      if (res.ok) {
+        // transforming the "res" variable to json
+        const json = await res.json();
+        // setting the state
+        setUsers(json.results); // results is the API array
+      } else {
+        setError("There was a problem fetching users. Please try again later.");
+        throw new Error(`HTTP error! Status: ${res.status}`);
+      }
     } catch (error) {
       console.error("Could not fetch users", error);
     } finally {
@@ -48,8 +54,6 @@ export default function Home() {
     fetchData();
   }, []);
 
-  if (loading) return <p>Loading...</p>;
-
   const lastPostIndex = currentPage * postsPerPage; // setting the last index
   const firstPostIndex = lastPostIndex - postsPerPage; // setting the first index
   const currentPosts = users.slice(firstPostIndex, lastPostIndex); // using slice to get starting index and last index
@@ -57,19 +61,25 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="flex flex-col gap-4 items-center">
-          { currentPosts.map((user) => ( // mapping the users via "user"
-            <div key={user.login?.uuid}>
-              {user.name?.first || "No first name available"}
-            </div>
-          )) }
-          <Pagination
-            totalPosts={users.length}
-            postsPerPage={postsPerPage}
-            setCurrentPage={setCurrentPage}
-            currentPage={currentPage}
-          />
-        </div>
+        { loading ? (
+          <BarLoader color="#5fee69" />
+        ) : error ? (
+          <div className="text-red-500">There is an error. Please try again later.</div>
+        ) : (
+          <div className="flex flex-col gap-4 items-center">
+            { currentPosts.map((user) => ( // mapping the users via "user"
+              <div key={user.login?.uuid}>
+                {user.name?.first || "No first name available"}
+              </div>
+            )) }
+            <Pagination
+              totalPosts={users.length}
+              postsPerPage={postsPerPage}
+              setCurrentPage={setCurrentPage}
+              currentPage={currentPage}
+            />
+          </div>
+        ) }
       </main>
     </div>
   );
